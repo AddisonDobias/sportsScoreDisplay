e54874440fd6e1a55ca2c2042e885e3e62b4cc47
Kshaya <shrinathrao97@gmail.com>
2025-07-12 22:29:03 -0500

    Do we have cache?

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,42 +1,85 @@
 import { useRef, useState } from "react";
 
-
-
 export function HelloWorld() {
     
-    // State setup
     const [randomInteger, setRandomInteger] = useState(0);
-    const count = useRef(0);
+    const isInCacheArr = useRef(false);
+    const cacheArray = useRef<number[]>([]);
+
+    const isInCacheMap = useRef(false);
+    const cacheMap = useRef<Record<number, boolean>>({});
 
     // Handle button click
     const handleClick = () => {
-        const min = 0;
+
+        //Generates a new random number to be set as randomInteger
         const max = 100;
-        const newRandVal = randNumber({startingNumber: randomInteger, min, max});
+        const newRandVal = randNumber({max});
+        
+        //Checks if newRandVal has been seen before
+        isInCacheArr.current = isInCachedArray(cacheArray.current, newRandVal);
+        isInCacheMap.current = isInCachedMap(cacheMap.current, newRandVal);
+        //Sets state causing re-render
         setRandomInteger(newRandVal);
-        count.current = count.current + 1;
     }
 
-    return (
-    <div>
-        <p>{"Random Number: " + randomInteger}</p>
-        <p>{"Count: " + count.current}</p>
 
-        <button onClick={handleClick}>Increase count</button>
-    </div>);
+    return (
+        <div>
+            <p>{"Random Number: " + randomInteger}</p>
+            <p>{"Is in array cache: " + isInCacheArr.current}</p>
+            <p>{"Is in array cache: " + isInCacheMap.current}</p>
+            <button onClick={handleClick}>Increase count</button>
+        </div>
+    );
 }
 
+
+//Takes in an map of previously seen cached values and returns if exists or adds it
+function isInCachedMap(cacheMap: Record<number, boolean>, numberToFind: number) {
+
+    //Check if record exists in hashmap
+    if (cacheMap[numberToFind]){
+        return true;
+    }
+
+    //Adds to the cacheMap
+    cacheMap[numberToFind] = true;
+
+    //Returns false
+    return false;
+
+}
+
+
+
+//Takes in an array of previously seen cached values and returns if exists or adds it
+function isInCachedArray(cacheArray: number[], numberToFind: number) {
+
+    // See if numberToFind is in cacheArray
+    for (let i = 0; i < cacheArray.length; i++) {
+    
+        // If it is return true
+        if(numberToFind === cacheArray[i])  {
+            return true;
+        }
+    
+    }
+
+    // If not add it to the array
+    cacheArray.push(numberToFind);
+
+    // Return false
+    return false;
+}
+
+
 type RandNumberParams = {
-    startingNumber: number;
-    min: number;
     max: number;
 }
 
 //Generates a random number given starting, min and max;
-function randNumber({startingNumber, min, max}: RandNumberParams) {
+function randNumber({max}: RandNumberParams) {
     const finalVal = Math.ceil(Math.random() * max);
-    
-    console.log("RandNumber: ", startingNumber, min, max);
-
     return finalVal;
 }
