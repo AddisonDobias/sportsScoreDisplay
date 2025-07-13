d368b79ea3303245beb8d3338556cc987359d970
Kshaya <shrinathrao97@gmail.com>
2025-07-13 16:52:20 -0500

    Remove unused vars and imports

--- src/components/BasketBallInfo/BasketBallInfo.tsx
+++ src/components/BasketBallInfo/BasketBallInfo.tsx
@@ -6,13 +6,11 @@ import { BasketballGameTile } from './BasketBallGameTile';
 export function BasketBallInfo() {
 
     const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
 
 
     //@ts-ignore
     useEffect(() => setData(exampleResponse.response), []); // Empty dependency array ensures this runs once on mount
 
-    if (error) return <div>Error: {error}</div>;
 
     return (
         <div>
